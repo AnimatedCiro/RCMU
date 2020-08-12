5be33aacabe4e753b6524e3a5f4d6c94c7231c5e
AnimatedCiro <cirodemartino95@yahoo.it>
2020-08-12 11:27:29 +0200

    Addedd Network manager for SyncVar

--- Assets/Script/Drive.cs
+++ Assets/Script/Drive.cs
@@ -1,9 +1,9 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
-using Mirror;
+//using Mirror;
 
-public class Drive : NetworkBehaviour
+public class Drive : MonoBehaviour
 {
     public Text verticale, orizzontale;
     public Joystick joystick;
@@ -13,28 +13,31 @@ public class Drive : NetworkBehaviour
     public Spaceship spaceship;
 
     private bool entered;
-
-
-    [SyncVar]
     public float Horizontal;
-
-    [SyncVar]
     public float Vertical;
+
+    public GameManagerScript gc;
+
 
     void Start()
     {
-        if(isServer){
+        /*if(isServer){
             this.gameObject.active=false;
-        }
+        }*/
         joystick = FindObjectOfType<Joystick>();
-        spaceship = FindObjectOfType<Spaceship>();
+        //spaceship = FindObjectOfType<Spaceship>();
+        gc = FindObjectOfType<GameManagerScript>();
     }
     
     // Update is called once per frame
     void Update()
     {
-        if (!isLocalPlayer)
-                return;
+
+        
+        /*
+        if (!isLocalPlayer){
+            return;
+        }*/
         
 
         if(button.pressed && !entered){
@@ -48,31 +51,32 @@ public class Drive : NetworkBehaviour
         if(comunicazione.pressed)
             inviaComunicazione();
 
-        if(spaceship !=null){
+       /* if(spaceship.power){
             analogico();
-        }
+        }*/
 
 
         orizzontale.text = "Orizzontale : "+joystick.Horizontal;
         verticale.text = "Verticale : "+joystick.Vertical;
     }
 
-    [Command]
+    //[Command]
     void accensione(){
-        spaceship.Switch_Power();
+        gc.increase();
+        //spaceship.Switch_Power();
         return;
     }
 
     
-    [Command]
+    //[Command]
     void analogico(){
-        spaceship.Set_Movement(joystick.Vertical, joystick.Horizontal);
+        //spaceship.GetComponent<Spaceship>().Set_Movement(joystick.Vertical, joystick.Horizontal);
         return;
     }
 
-    [Command]
+    //[Command]
     void inviaComunicazione(){
-        spaceship.comunicazione();
+        //spaceship.comunicazione();
         return;
     }
     
---
+++ Assets/Script/GameManagerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class GameManagerScript : NetworkBehaviour
+{
+    [SyncVar]
+    public int Test42 = 0;
+
+    public GameObject navicella;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(isServer){
+            GameObject panello = FindObjectOfType<Drive>().gameObject;
+            Destroy(panello);
+
+            navicella = FindObjectOfType<Spaceship>().gameObject;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+    [Command]
+    public void increase(){
+        Test42++;
+        return;
+    }
+}
